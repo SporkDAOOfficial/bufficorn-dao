3621f9f4bdb746ad82446338315dcfcc4c22305a
earth2travis <travis.mccutcheon@gmail.com>
2023-05-18 17:46:41 -0500

    Update titles, subtitles, and descriptions

--- src/legos/forms.ts
+++ src/legos/forms.ts
@@ -1,15 +1,15 @@
-import { FIELD, TX } from "@daohaus/moloch-v3-legos";
-import { CustomFormLego } from "./fieldConfig";
-import { APP_TX } from "./tx";
+import { FIELD, TX } from '@daohaus/moloch-v3-legos';
+import { CustomFormLego } from './fieldConfig';
+import { APP_TX } from './tx';
 
 const PROPOSAL_SETTINGS_FIELDS = [FIELD.PROPOSAL_EXPIRY, FIELD.PROP_OFFERING];
 
 export const APP_FORM: Record<string, CustomFormLego> = {
   INIT_VOTE: {
-    id: "INIT_VOTE",
-    title: "Initiate Vote",
-    subtitle: "Intiate Vote Proposal",
-    description: "Ratify on-chain using a DAO proposal.",
+    id: 'INIT_VOTE',
+    title: 'Initiate Vote',
+    subtitle: 'Signal Proposal',
+    description: 'Ratify vote with an on-chain signal.',
     requiredFields: { title: true, description: true },
     log: true,
     tx: APP_TX.INIT_VOTE,
@@ -21,10 +21,10 @@ export const APP_FORM: Record<string, CustomFormLego> = {
     ],
   },
   FUND_COMPANY: {
-    id: "FUND_COMPANY",
-    title: "Fund Company",
-    subtitle: "Funding Proposal",
-    description: "Send ERC-20 tokens from the DAO treasury.",
+    id: 'FUND_COMPANY',
+    title: 'Fund Company',
+    subtitle: 'Funding Proposal',
+    description: 'Send ERC-20 tokens from the Treasury.',
     tx: TX.ISSUE_ERC20,
     requiredFields: {
       title: true,
@@ -33,26 +33,25 @@ export const APP_FORM: Record<string, CustomFormLego> = {
       recipient: true,
     },
     fields: [
-      { ...FIELD.TITLE, label: "Title" },
+      { ...FIELD.TITLE, label: 'Title' },
       FIELD.DESCRIPTION,
       FIELD.LINK,
       {
-        id: "recipient",
-        type: "input",
-        label: "Recipient",
-        info: "Address to receive the tokens",
-        placeholder: "0x...",
+        id: 'recipient',
+        type: 'input',
+        label: 'Recipient',
+        info: 'Address to receive the tokens',
+        placeholder: '0x...',
       },
       FIELD.REQUEST_TOKEN,
       ...PROPOSAL_SETTINGS_FIELDS,
     ],
   },
   BECOME_PARTNER: {
-    id: "BECOME_PARTNER",
-    title: "Become Partner",
-    description:
-      "Request voting or non-voting tokens from the DAO in exchange for tokens.",
-    subtitle: "Token Proposal",
+    id: 'BECOME_PARTNER',
+    title: 'Become Partner',
+    subtitle: 'Partner Proposal',
+    description: 'Request BVSTK or BVSTK-LOOT in exchange for ERC-20 tokens.',
     tx: TX.TOKENS_FOR_SHARES,
     requiredFields: {
       title: true,
@@ -66,23 +65,23 @@ export const APP_FORM: Record<string, CustomFormLego> = {
       FIELD.LINK,
       {
         ...FIELD.TO_WEI,
-        label: "Voting Token Requested",
-        id: "sharesRequested",
+        label: 'Voting Token Requested',
+        id: 'sharesRequested',
       },
       {
         ...FIELD.TO_WEI,
-        label: "Non-Voting Token Requested",
-        id: "lootRequested",
+        label: 'Non-Voting Token Requested',
+        id: 'lootRequested',
       },
       FIELD.TRIBUTE,
       ...PROPOSAL_SETTINGS_FIELDS,
     ],
   },
   ADD_PARTNER: {
-    id: "ADD_PARTNER",
-    title: "Add Partner",
-    subtitle: "Token Proposal",
-    description: "Request voting or non-voting tokens from the DAO.",
+    id: 'ADD_PARTNER',
+    title: 'Add Partner',
+    subtitle: 'Partner Proposal',
+    description: 'Send BVSTK or BVSTK-LOOT tokens.',
     tx: TX.ISSUE,
     requiredFields: {
       title: true,
@@ -96,21 +95,21 @@ export const APP_FORM: Record<string, CustomFormLego> = {
       FIELD.DESCRIPTION,
       FIELD.LINK,
       {
-        id: "recipient",
-        type: "input",
-        label: "Recipient",
-        expectType: "ethAddress",
-        placeholder: "0x...",
+        id: 'recipient',
+        type: 'input',
+        label: 'Recipient',
+        expectType: 'ethAddress',
+        placeholder: '0x...',
       },
       {
         ...FIELD.TO_WEI,
-        label: "Voting Token Requested",
-        id: "sharesRequested",
+        label: 'Voting Token Requested',
+        id: 'sharesRequested',
       },
       {
         ...FIELD.TO_WEI,
-        label: "Non-Voting Token Requested",
-        id: "lootRequested",
+        label: 'Non-Voting Token Requested',
+        id: 'lootRequested',
       },
       ...PROPOSAL_SETTINGS_FIELDS,
     ],
